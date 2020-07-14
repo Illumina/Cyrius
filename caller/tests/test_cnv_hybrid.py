caa0fe5baef81d16aff69e8e138fd39a5db4e011
Xiao Chen <chenxecho@gmail.com>
2020-07-13 17:15:38 -0700

    Updated tests

--- caller/tests/test_cnv_hybrid.py
+++ caller/tests/test_cnv_hybrid.py
@@ -25,9 +25,10 @@ from collections import namedtuple
 from ..cnv_hybrid import get_cnvtag
 
 
-TOTAL_SITE = 116
+TOTAL_SITE = 117
 cn_regions = namedtuple(
-    "cn_regions", "rep exon9_and_downstream exon9_to_intron1 intron1_upstream"
+    "cn_regions",
+    "rep exon9_and_downstream exon9_to_intron4 intron4_to_intron1 intron1_upstream",
 )
 
 
@@ -43,7 +44,7 @@ class TestCallCNVGroup(object):
         )
         assert cnvtag[0] == "cn3"
 
-        cn_call_per_site = [2] * 8 + [3] * (TOTAL_SITE - 8)
+        cn_call_per_site = [2] * 9 + [3] * (TOTAL_SITE - 9)
         rawv = cn_call_per_site
         exon9gc_call_stringent = 2
         spacer_cn = 3
@@ -51,9 +52,21 @@ class TestCallCNVGroup(object):
             total_cn, rawv, cn_call_per_site, exon9gc_call_stringent, spacer_cn
         )
         assert cnvtag[0] == "exon9hyb"
-        assert cnvtag[1] == cn_regions(2, 2, 3, 3)
+        assert cnvtag[1] == cn_regions(2, 2, 3, 3, 3)
 
-        cn_call_per_site = [2] * 3 + [1] * 4 + [2] + [3] * (TOTAL_SITE - 8)
+        # *10D
+        cn_call_per_site = [2] * 7 + [3] * (TOTAL_SITE - 7)
+        rawv = cn_call_per_site
+        exon9gc_call_stringent = 3
+        spacer_cn = 3
+        cnvtag = get_cnvtag(
+            total_cn, rawv, cn_call_per_site, exon9gc_call_stringent, spacer_cn
+        )
+        assert cnvtag[0] == "cn3"
+        assert cnvtag[1] == cn_regions(2, 3, 3, 3, 3)
+
+        # conversion to CYP2D7 downstream of the gene
+        cn_call_per_site = [2] * 3 + [1] * 4 + [2, 2] + [3] * (TOTAL_SITE - 9)
         rawv = cn_call_per_site
         exon9gc_call_stringent = 2
         spacer_cn = 4
@@ -61,7 +74,7 @@ class TestCallCNVGroup(object):
             total_cn, rawv, cn_call_per_site, exon9gc_call_stringent, spacer_cn
         )
         assert cnvtag[0] == "exon9hyb"
-        assert cnvtag[1] == cn_regions(2, 2, 3, 3)
+        assert cnvtag[1] == cn_regions(2, 2, 3, 3, 3)
 
         cn_call_per_site = [2] * 74 + [3] * (TOTAL_SITE - 74)
         rawv = cn_call_per_site
@@ -83,7 +96,7 @@ class TestCallCNVGroup(object):
         assert cnvtag[0] == "dup_star68"
 
         total_cn = 4
-        cn_call_per_site = [2] * 3 + [1] * 5 + [2] * (TOTAL_SITE - 8)
+        cn_call_per_site = [2] * 3 + [1] * 6 + [2] * (TOTAL_SITE - 9)
         rawv = cn_call_per_site
         exon9gc_call_stringent = 1
         spacer_cn = 3
@@ -91,3 +104,26 @@ class TestCallCNVGroup(object):
             total_cn, rawv, cn_call_per_site, exon9gc_call_stringent, spacer_cn
         )
         assert cnvtag[0] == "exon9hyb_star5"
+
+        # exon 9 gene conversion
+        total_cn = 4
+        cn_call_per_site = [2] * 7 + [1] * 2 + [2] * (TOTAL_SITE - 9)
+        rawv = cn_call_per_site
+        exon9gc_call_stringent = 1
+        spacer_cn = 2
+        cnvtag = get_cnvtag(
+            total_cn, rawv, cn_call_per_site, exon9gc_call_stringent, spacer_cn
+        )
+        assert cnvtag[0] == "cn2"
+
+        # fusion deletion with breakpoint in intron4
+        total_cn = 3
+        cn_call_per_site = [2] * 40 + [1] * (TOTAL_SITE - 40)
+        rawv = cn_call_per_site
+        exon9gc_call_stringent = 2
+        spacer_cn = 1
+        cnvtag = get_cnvtag(
+            total_cn, rawv, cn_call_per_site, exon9gc_call_stringent, spacer_cn
+        )
+        assert cnvtag[0] == "star13intron1"
+        assert cnvtag[1] == cn_regions(2, 2, 2, 1, 1)
